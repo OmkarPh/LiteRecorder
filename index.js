e5e651667f9ee89ff8c6f925c09b92c70fe220bf
OmkarPh <omkarphansopkar@gmail.com>
2020-11-18 16:59:57 +0530

    Added support for stream from microphone

--- index.js
+++ index.js
@@ -1,113 +1,45 @@
-const vid = document.querySelector("#recorded");
-const start = document.querySelector('#start');
-const stop = document.querySelector('#stop');
-const volToggle = document.querySelector('#toggleVolume');
-let alertPermissionCount = 0;
-let recorder, stream, recordedVideo;
-let isRecording = false;
-
+// Preliminary checks for API support
 if(!navigator.mediaDevices){
     showErrorMessage("https");
-    thisFunctionDoesntExistHenceWillCauseErrorAndStopExecutionOfFurtherJS(":)");
+    thisFunctionDoesntExistHenceWillCauseErrorAndStopExecutionOfFurtherJS(":-)");
 }
 
 if(navigator.mediaDevices.ondevicechange !== null){
     showErrorMessage();
-    thisFunctionDoesntExistHenceWillCauseErrorAndStopExecutionOfFurtherJS(":)");
+    thisFunctionDoesntExistHenceWillCauseErrorAndStopExecutionOfFurtherJS(":-)");
 }
 
 
+const vid = document.querySelector("#recorded");
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+const micToggle = document.querySelector('#toggleMic');
+const volToggle = document.querySelector('#toggleAudio');
+
+let alertPermissionCount = 0;
+let recorder, stream, recordedVideo, micStream;
+let isRecording = false;
+
+// Utility functions
 function getFormattedTime(downloadable = false){
     let date = new Date();
     if(downloadable)
         return `on ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} at ${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`;
     return `on ${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
-function stopRecord(){
-    console.log("Stopped recording at ",getFormattedTime());
-    if(isRecording){
-        stop.setAttribute("disabled", true);
-        start.removeAttribute("disabled");
-        isRecording = false;
-    }
-    
-    start.setAttribute("class", "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded");
-    stop.setAttribute("class","bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed");
-    vid.setAttribute("controls","");
+function toggleModal(modalID, title, message=""){
+    let modal = temp = document.getElementById(modalID);
+
+    modal.children[0].children[0].children[0].children[0].innerHTML = title;
+    modal.children[0].children[0].children[1].children[0].innerHTML = message;
+
+    modal.classList.toggle("hidden");
+    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+
+    modal.classList.toggle("flex");
+    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+    return true;
 }
-async function startRecord(audioEnabled){
-    try{
-        start.setAttribute("class","bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed");
-        stop.setAttribute("class", "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded");
-
-        vid.removeAttribute("controls");
-        stream = await navigator.mediaDevices.getDisplayMedia({ video:{cursor: 'always'}, audio: audioEnabled });
-        
-        vid.srcObject = stream;
-        vid.muted = true;
-        console.log("Started recording at ",getFormattedTime());
-        recorder = new MediaRecorder(stream);
-        const chunks = [];
-        recorder.ondataavailable = e => chunks.push(e.data);
-        recorder.onstop = e => {
-            stopRecord();
-            const completeBlob = new Blob(chunks, { type: chunks[0].type });
-            vid.srcObject = null;
-            vid.src = URL.createObjectURL(completeBlob);
-            let a = document.getElementById('downloadVideo');
-            a.style.visibility = "visible";
-            a.href        = vid.getAttribute("src");
-            a.download    = `Recorded_Video_${getFormattedTime(true)}.mp4`;
-        };
-        recorder.start();
-    }catch(err){
-        stopRecord();
-        if(err.name == "NotAllowedError"){
-            if(alertPermissionCount == 0){
-                toggleModal("mainModal",
-                            "Please provide permission to record !",
-                            `Javascript of page cannot process without your permission to share screen, <br> Select Entire Screen or Particular window/browser tab and click Share in browser dialog <br> Tick checkbox saying 'Share Audio' if Mic turned on !`
-                            );
-                alertPermissionCount=1;
-            }
-            return;
-        }
-        if(err.message == "Could not start audio source")
-            return toggleModal("mainModal",
-                                "Couldn't find audio source on your device !",
-                                "Check if your mic is working properly !");
-            toggleModal("mainModal",
-                        "Something wrong happened !",
-                        "Your device doesn't seem to be compatible with this feature "
-                        );
-        console.log("Error: "+err);
-    }
-}
-
-start.addEventListener("click", function(event){
-    start.setAttribute("disabled", true);
-    stop.removeAttribute("disabled");
-    isRecording = true;
-    startRecord(document.getElementById("toggleVolume").checked);
-});
-stop.addEventListener("click", function(event){
-    stop.setAttribute("disabled", true);
-    start.removeAttribute("disabled");
-    isRecording = false;
-    recorder.stop();
-    stream.getVideoTracks()[0].stop();
-});
-function toggleAudio(){
-    let isEnabled = volToggle.checked;
-    if(isEnabled)
-        document.getElementById("volumeStatus").innerHTML = '<i class="fas fa-microphone-alt text-xl" style="color:blue;"></i>';
-    else
-        document.getElementById("volumeStatus").innerHTML = '<i class="fas fa-microphone-alt-slash text-xl" style="color: black;"></i>';
-}
-volToggle.addEventListener("change", toggleAudio);
-
-
-
 function showErrorMessage(type){
     
     document.body.innerHTML = `<div style="font-size: 25px; font-style: oblique;">
@@ -137,20 +69,144 @@ function showErrorMessage(type){
     document.body.style = "height: 100vh; max-height: 90vh; background-image:url('bg.svg');"
 
 }
+let dstt;
+
+const mergeAudioStreams = (desktopStream, voiceStream) => {
+    const context = new AudioContext();
+    dstt = desktopStream;
+    if(desktopStream.getAudioTracks().length == 0){
+        // User didn't share desktop audio so, output is directly returned as voicStream i.e. mic audio
+        return voiceStream.getAudioTracks();
+    }
+    if(voiceStream.getAudioTracks().length == 0){
+        // User didn't share mic audio so, output is directly returned as desktopStream i.e. system audio
+        return voiceStream.getAudioTracks();
+    }
+
+    // Create a couple of sources
+    const source1 = context.createMediaStreamSource(desktopStream);
+    const source2 = context.createMediaStreamSource(voiceStream);
+    const destination = context.createMediaStreamDestination();
+    
+    const desktopGain = context.createGain();
+    const voiceGain = context.createGain();
+      
+    desktopGain.gain.value = 0.7;
+    voiceGain.gain.value = 0.7;
+     
+    source1.connect(desktopGain).connect(destination);
+    // Connect source2
+    source2.connect(voiceGain).connect(destination);
+      
+    return destination.stream.getAudioTracks();
+  };
 
 
-function toggleModal(modalID, title, message=""){
-    let modal = temp = document.getElementById(modalID);
 
-    modal.children[0].children[0].children[0].children[0].innerHTML = title;
-    modal.children[0].children[0].children[1].children[0].innerHTML = message;
 
-    modal.classList.toggle("hidden");
-    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
 
-    modal.classList.toggle("flex");
-    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
-    return true;
+
+
+
+function preStopRecord(){
+    console.log("Stopped recording at ",getFormattedTime());
+    
+    stop.setAttribute("disabled", true);
+    start.removeAttribute("disabled");
+    
+    
+    start.setAttribute("class", "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded");
+    stop.setAttribute("class","bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed");
+    vid.setAttribute("controls","");
 }
+async function startRecord(audioEnabled, micEnabled){
+    try{
+        start.setAttribute("class","bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed");
+        stop.setAttribute("class", "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded");
+
+        vid.removeAttribute("controls");
+        stream = await navigator.mediaDevices.getDisplayMedia({ video:{cursor: 'always'}, audio: audioEnabled });
+        if(micEnabled){
+            micStream = null;
+            micStream = await navigator.mediaDevices.getUserMedia({video: false, audio: true});
+            const tracks = [
+                ...stream.getVideoTracks(), 
+                ...mergeAudioStreams(stream, micStream)
+            ];              
+            stream = new MediaStream(tracks);
+        }
+        vid.srcObject = stream;
+        vid.muted = true;
+        console.log("Started recording at ",getFormattedTime());
+        recorder = new MediaRecorder(stream, {mimeType: 'video/webm; codecs=vp8,opus'});
+        const chunks = [];
+        recorder.ondataavailable = e => chunks.push(e.data);
+        recorder.onstop = e => {
+            preStopRecord();
+            const completeBlob = new Blob(chunks, { type: 'video/webm' });
+            vid.srcObject = null;
+            vid.src = URL.createObjectURL(completeBlob);
+            vid.muted = false;
+            let a = document.getElementById('downloadVideo');
+            a.style.visibility = "visible";
+            a.href        = vid.getAttribute("src");
+            a.download    = `Recorded_Video_${getFormattedTime(true)}.mp4`;
+        };
+        recorder.start();
+    }catch(err){
+        preStopRecord();
+        if(err.name == "NotAllowedError"){
+            if(alertPermissionCount == 0){
+                toggleModal("mainModal",
+                            "Please provide permission to record !",
+                            `Javascript of page cannot process without your permission to share screen, <br> Select Entire Screen or Particular window/browser tab and click Share in browser dialog <br> Tick checkbox saying 'Share Audio' if Mic turned on !`
+                            );
+                alertPermissionCount=1;
+            }
+            return;
+        }
+        if(err.message == "Could not start audio source")
+            return toggleModal("mainModal",
+                                "Couldn't find audio output on your device !",
+                                "Check if your speaker/headset is working properly !");
+        toggleModal("mainModal",
+                        "Something wrong happened !",
+                        "Your device doesn't seem to be compatible with this feature "
+                        );
+        console.log("Error: ");
+        console.log(err);
+    }
+}
+
+start.addEventListener("click", function(event){
+    start.setAttribute("disabled", true);
+    stop.removeAttribute("disabled");
+    isRecording = true;
+    startRecord(document.getElementById("toggleAudio").checked, document.getElementById("toggleMic").checked);
+});
+stop.addEventListener("click", function(event){
+    stop.setAttribute("disabled", true);
+    start.removeAttribute("disabled");
+    isRecording = false;
+    recorder.stop();
+    stream.getVideoTracks()[0].stop();
+});
+function toggleMic(){
+    let isEnabled = micToggle.checked;
+    if(isEnabled)
+        document.getElementById("micStatus").innerHTML = '<i class="fas fa-microphone-alt text-xl" style="color:blue;"></i>';
+    else
+        document.getElementById("micStatus").innerHTML = '<i class="fas fa-microphone-alt-slash text-xl" style="color: black;"></i>';
+}
+function toggleVolume(){
+    let isEnabled = volToggle.checked;
+    if(isEnabled)
+        document.getElementById("volumeStatus").innerHTML = '<i class="fas fa-volume text-xl" style="color:blue;"></i>';
+    else
+        document.getElementById("volumeStatus").innerHTML = '<i class="fas fa-volume-mute text-xl" style="color: black;"></i>';
+}
+micToggle.addEventListener("change", toggleMic);
+volToggle.addEventListener("change", toggleVolume);
+
 
 
